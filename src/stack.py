d36f0684e3f1e3d2d5de525bb63c73f1dd1a44f6
Kennedy Mindeman <kennedymindeman@gmail.com>
2023-04-01 11:27:42 -0500

    Add stack push method

--- src/stack.py
+++ src/stack.py
@@ -9,3 +9,15 @@ class Stack:
 
     def __init__(self, iterable: Optional[Iterable] = None):
         self._list = list(iterable) if iterable else []
+
+    def push(self, item: object) -> bool:
+        """Push an item onto the stack
+
+        :param item: The item to add to the stack
+        :return: True if adding the item was successful false otherwise
+        """
+        try:
+            self._list.append(item)
+            return True
+        except MemoryError:
+            return False
