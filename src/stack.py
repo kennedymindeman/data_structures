efcb4fc61f4d8cb78179c1f21ebcc987eee15a22
Kennedy Mindeman <kennedymindeman@gmail.com>
2023-04-01 11:11:26 -0500

    Add option to pass iterable to stack constructor

--- src/stack.py
+++ src/stack.py
@@ -1,8 +1,11 @@
 """Stack class file"""
 
 
+from typing import Iterable, Optional
+
+
 class Stack:
     """Stack class that supports LIFO operations"""
 
-    def __init__(self):
-        pass
+    def __init__(self, iterable: Optional[Iterable] = None):
+        self._list = list(iterable) if iterable else []
