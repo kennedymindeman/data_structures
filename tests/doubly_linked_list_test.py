1fa48382dec9ff39dae9af84db3ca2c1ac902c8f
Kennedy Mindeman <kennedymindeman@gmail.com>
2023-04-04 21:55:00 -0500

    Add constructor test for doubly linked list with iterator

--- tests/doubly_linked_list_test.py
+++ tests/doubly_linked_list_test.py
@@ -5,3 +5,8 @@ from src.doubly_linked_list import DoublyLinkedList
 def test_doubly_linked_list_constructor():
     """Tests the constructor of a linked list"""
     assert isinstance(DoublyLinkedList(), DoublyLinkedList)
+
+
+def test_doublye_linked_list_constructor_with_iterable():
+    """Tests the constructor of a linked list with an iterable"""
+    assert isinstance(DoublyLinkedList([1, 2, 3]), DoublyLinkedList)
