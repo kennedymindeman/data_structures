bb7129a9df414697860d7d76badf2ede352b82a4
Kennedy Mindeman <kennedymindeman@gmail.com>
2023-04-04 21:40:20 -0500

    Implement insert method in doubly linked list

--- src/doubly_linked_list.py
+++ src/doubly_linked_list.py
@@ -21,4 +21,40 @@ class DoublyLinkedList:
             self.next_node = next_node
 
     def __init__(self, iterable: Optional[Iterable] = None) -> None:
-        pass
+        self.head = None
+        self.tail = None
+        self._length = 0
+        for item in iterable or []:
+            self.insert(item)
+
+    def insert(self, val: object, index=0) -> None:
+        """Inserts an item into the list at a specifed position
+
+        :param val: The value to insert into the list
+        :param index: The index at which to insert the item, defaults to 0
+        """
+        self._length += 1
+        if self.head is None and self.tail is None:
+            self.head = DoublyLinkedList.Node(val, self.head)
+            self.tail = self.head
+            return
+
+        if index > self._length or self.head is None:
+            raise Exception
+
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next_node
+            if curr is None:
+                raise Exception
+
+        new_node = DoublyLinkedList.Node(val, curr.prev_node, curr)
+        curr.prev_node = new_node
+        if isinstance(new_node.prev_node, DoublyLinkedList.Node):
+            new_node.prev_node.next_node = new_node
+
+        if new_node.next_node is None:
+            self.tail = new_node
+
+        if new_node.prev_node is None:
+            self.head = new_node
